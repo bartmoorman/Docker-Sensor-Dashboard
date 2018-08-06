deb37d540ef78b6e7f2ac62d666644c31022c645
Bart Moorman <github@id10t.us>
2018-08-05 23:12:59 -0600

    Setup: Enable configuring notifications

--- htdocs/setup.php
+++ htdocs/setup.php
@@ -43,8 +43,51 @@ $dashboard = new Dashboard(false, true, false, true);
               </div>
               <div class='form-row'>
                 <div class='form-group col'>
-                  <label>Role <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup> <sup class='text-info' data-toggle='tooltip' title='Informational'>*</sup></label>
+                  <label>Role <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
                   <input class='form-control' id='role' type='text' name='role' value='admin' readonly required>
+                </div>
+              </div>
+              <div class='form-row'>
+                <div class='form-group col'>
+                  <label>Pushover User Key</label>
+                  <input class='form-control' id='pushover_user' type='text' name='pushover_user' minlegth='30' maxlength='30' pattern='[A-Za-z0-9]{30}'>
+                </div>
+                <div class='form-group col'>
+                  <label>Pushover App. Token</label>
+                  <input class='form-control' id='pushover_token' type='text' name='pushover_token' minlegth='30' maxlength='30' pattern='[A-Za-z0-9]{30}'>
+                </div>
+              </div>
+              <div class='form-row'>
+                <div class='form-group col'>
+                  <label>Pushover Sound <sup><a target='_blank' href='https://pushover.net/api#sounds'>Listen</a></sup></label>
+                  <select class='form-control' id='pushover_sound' name='pushover_sound'>
+                    <option value=''>User Default</option>
+<?php
+foreach ($dashboard->getSounds() as $value => $text) {
+  echo "                    <option value='{$value}'>{$text}</option>" . PHP_EOL;
+}
+?>
+                  </select>
+                </div>
+                <div class='form-group col'>
+                  <label>Pushover Priority</label>
+                  <select class='form-control id-pushover_priority' id='pushover_priority' name='pushover_priority'>
+<?php
+for ($priority = -2; $priority <= 2; $priority++) {
+  echo "                    <option value='{$priority}'>{$priority}</option>" . PHP_EOL;
+}
+?>
+                  </select>
+                </div>
+              </div>
+              <div class='form-row id-required'>
+                <div class='form-group col'>
+                  <label>Pushover Retry <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
+                  <input class='form-control id-pushover_retry' id='pushover_retry' type='number' name='pushover_retry' min='30' required>
+                </div>
+                <div class='form-group col'>
+                  <label>Pushover Expire <sup class='text-danger' data-toggle='tooltip' title='Required'>*</sup></label>
+                  <input class='form-control id-pushover_expire' id='pushover_expire' type='number' name='pushover_expire' max='10800' required>
                 </div>
               </div>
             </div>
@@ -62,11 +105,20 @@ $dashboard = new Dashboard(false, true, false, true);
       $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
 
+        $('select.id-pushover_priority').val(0);
+        $('div.id-required').addClass('d-none');
+        $('input.id-pushover_retry').val(60);
+        $('input.id-pushover_expire').val(3600);
+
         $('div.modal').modal({backdrop: false, keyboard: false});
+
+        $('select.id-pushover_priority').change(function() {
+          $('div.id-required').toggleClass('d-none', $(this).val() != 2 ? true : false);
+        });
 
         $('form').submit(function(e) {
           e.preventDefault();
-          $.post('src/action.php', {"func": "createUser", "username": $('#username').val(), "password": $('#password').val(), "first_name": $('#first_name').val(), "last_name": $('#last_name').val(), "role": $('#role').val()})
+          $.post('src/action.php', {"func": "createUser", "username": $('#username').val(), "password": $('#password').val(), "first_name": $('#first_name').val(), "last_name": $('#last_name').val(), "pushover_user": $('#pushover_user').val(), "pushover_token": $('#pushover_token').val(), "pushover_priority": $('#pushover_priority').val(), "pushover_retry": $('#pushover_retry').val(), "pushover_expire": $('#pushover_expire').val(), "pushover_sound": $('#pushover_sound').val(), "role": $('#role').val()})
             .done(function(data) {
               if (data.success) {
                 location.href = '<?php echo dirname($_SERVER['PHP_SELF']) ?>';
