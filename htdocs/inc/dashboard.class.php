74de5da471720d0055ff596a137cab3bac889044
Bart Moorman <github@id10t.us>
2019-03-02 00:51:59 -0700

    Configure GC

--- htdocs/inc/dashboard.class.php
+++ htdocs/inc/dashboard.class.php
@@ -14,6 +14,8 @@ class Dashboard {
     session_start([
       'save_path' => '/config/sessions',
       'name' => '_sess_dashboard',
+      'gc_probability' => 1,
+      'gc_divisor' => 1000,
       'gc_maxlifetime' => 60 * 60 * 24 * 7,
       'cookie_lifetime' => 60 * 60 * 24 * 7,
       'cookie_secure' => true,
